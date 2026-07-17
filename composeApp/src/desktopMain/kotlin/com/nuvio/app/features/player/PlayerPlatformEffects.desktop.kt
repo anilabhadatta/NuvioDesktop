c294a6d94dbd2db48ac1cc94bee0686ecca41e15
Anilabha Datta <anilabhadatta@gmail.com>
2026-07-18 04:18:30 +0530

    Revert "build fix"
    This reverts commit d49e7eaaa7b5bf85e0e51cef24e15705f0eb37bc.
    
    Revert "build fix"
    
    This reverts commit 75915f1ac12f8399967c9b027e48cd16e894bf97.

--- composeApp/src/desktopMain/kotlin/com/nuvio/app/features/player/PlayerPlatformEffects.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/nuvio/app/features/player/PlayerPlatformEffects.desktop.kt
@@ -29,7 +29,7 @@ actual fun EnterImmersivePlayerMode(keepScreenAwake: Boolean) {
 @Composable
 actual fun ManagePlayerPictureInPicture(
     isPlaying: Boolean,
-    videoSize: IntSize,
+    playerSize: IntSize,
 ) = Unit
 
 @Composable
--- composeApp/src/desktopMain/kotlin/com/nuvio/app/features/watchprogress/CurrentDateProvider.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/nuvio/app/features/watchprogress/CurrentDateProvider.desktop.kt
@@ -4,15 +4,4 @@ import java.time.LocalDate
 
 actual object CurrentDateProvider {
     actual fun todayIsoDate(): String = LocalDate.now().toString()
-
-    actual fun localStartOfDayEpochMs(isoDate: String): Long? {
-        return try {
-            LocalDate.parse(isoDate)
-                .atStartOfDay(java.time.ZoneId.systemDefault())
-                .toInstant()
-                .toEpochMilli()
-        } catch (e: Exception) {
-            null
-        }
-    }
 }
